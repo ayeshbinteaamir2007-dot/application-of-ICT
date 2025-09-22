168f638c5ce79e2913dd140ee1aa91d6d27fb4fa
ayeshbinteaamir2007-dot <ayeshbinteaamir2007@gmail.com>
2025-09-22 13:38:14 +0500

    Add files via upload

---
+++ labtask.py
@@ -0,0 +1,12 @@
+name = 'ayesh'
+age = 18
+date_of_birth = '17/4/2007'
+height = 5.5
+student = True
+
+print (name)
+print (age)
+print (date_of_birth)
+print (height)
+print ('studnt is present:', student)
+       
---
+++ labtask3.py
@@ -0,0 +1,9 @@
+marks = int(input(' enter number 1-100'))
+if marks >=90 and marks<= 100:
+    print('grade a')
+elif marks >=75 and marks< 90:
+    print('grade B')
+elif marks >= 50 and marks< 75:
+    print('grade C')
+else:
+    print('grade F')
