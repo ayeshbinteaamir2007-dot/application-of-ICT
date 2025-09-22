273e6be5f4dbac3fca43d8f2aa46947a10b64963
DotHashir <78808939+DotHashir@users.noreply.github.com>
2025-09-22 22:18:29 +0500

    Add files via upload

---
+++ Hashir/Lab03/GradingSystem.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter your marks (0-100)"))
+
+if marks >= 90:
+    grade = 'A'
+
+elif marks >= 75 and marks < 90:
+    grade = 'B'
+
+elif marks >= 50 and marks < 75:
+    garde = 'C'
+
+else:
+    grade = 'F'
+
+print("Your grade is", grade)
